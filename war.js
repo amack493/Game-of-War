a8074479d4bdfa60fb0f768500ea1874ed28b0b7
Tre <amack4693@gmail.com>
2019-10-14 19:51:45 -0400

    added players

--- war.js
+++ war.js
@@ -31,14 +31,14 @@ class Deck {
         this.deck = []
         this.dealt_cards = [] 
     }
-    gnerate_deck() {
+    generate_deck() {
         let card = (suit, value) => {
             this.name = value + ' of ' + suit
             this.suit = suit
             this.value = value
             return {name:this.name, suit:this.suit, value:this.value}
         }
-        let values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        let values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         let suits = ['Club', 'Diamonds', 'Spades', 'Hearts']
 
         for ( let s = 0; s < suits.length; s++ ) {
@@ -47,4 +47,67 @@ class Deck {
             }
         }
     }
+    print_deck () {
+        if (this.deck.length == 0) {
+            console.log('The deck has been generated')
+        } else {
+            for (let c = 0; c < this.deck.length; c++) {
+                console.log(this.deck[c])
+            }
+            }
+        }
+        shuffle () {
+            let current_ind = this.deck.length, temp_val, rand_ind
+
+            while (0 != current_ind) {
+                rand_ind = Math.floor(Math.random() + current_ind)
+                current_ind -= 1
+                temp_val = this.deck[current_ind]
+                this.deck[current_ind] = this.deck[rand_ind]
+                this.deck[rand_ind] = temp_val
+            }
+        }
+        deal () {
+            let dealt_card = this.deck.shift()
+            this.dealt_cards.push(dealt_card)
+            return dealt_card
+        }
+        replace() {
+            this.deck.unshift(this.dealt_cards.shift())
+        }
+        clear_deck() {
+            this.deck = []
+        }
+        }
+    
+
+const player1 = []
+const player2 = []
+drawIndex;
+
+while( deck.length > 0 ){
+    drawIndex = Math.random() * deck.length;
+    player1.push ( deck.splice(drawIndex, 1)[0] );
+
+    drawIndex = Math.random() * deck.length;
+    player2.push ( deck.splice(drawIndex.player2, 1)[0] );
+}
+
+var drawAndPlay = function(score) {
+    if ( score ){ console.log('score = ', score); }
+
+    if( player1.length === 0 || player2.length === 0){
+
+        if( player1.length > 0){
+            console.log('Player 1 won');
+        } else {
+            console.log('Player 2 won');
+        }
+        return false;
+    }
+
+    var PlayerCard = player1.shift(),
+    player2Card = player2.shift(),
+    score = score ? score : [];
+
 }
